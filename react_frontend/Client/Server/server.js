0d07c95e5118f7644f7e422496f9e4fd9d13f7cd
Moin Vinchhi <moin.v@ahduni.edu.in>
2024-07-09 12:49:23 +0530

    commit_9_july

---
+++ react_frontend/Client/Server/server.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const PORT = 5000
+const cors = require('cors')
+const app = express()
+const uri = "mongodb://localhost:27017/students";
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('Mongodb is connected'))
+.catch((err) => console.error('Mongodb connection error', err));
+
+const db = mongoose.connection;
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.post('/register', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ error: "Email already exists" });
+    }
+    const newUser = new User({
+      name,
+      email,
+      password
+    })
+    const savedUser = await newUser.save()
+    res.status(201).json(savedUser)
+  } catch (error) {
+    console.error('Error during registration', error)
+    res.status(500).json({ error: "inter server error" })
+  }
+})
+
+app.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    if (user.password!== password) {
+      return res.status(401).json({ error: "Invalid password" });
+    }
+    res.status(200).json({ message: "Login successful" });
+  } catch (error) {
+    console.error('Error during login', error)
+    res.status(500).json({ error: "inter server error" })
+  }
+})
+
+app.listen(PORT);
