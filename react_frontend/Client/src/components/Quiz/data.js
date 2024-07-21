3957c9a72df439d8fe595cbce2616610b287e05e
Moin Vinchhi <moin.v@ahduni.edu.in>
2024-07-22 00:03:04 +0530

    quiz

--- react_frontend/Client/src/components/Quiz/data.js
+++ react_frontend/Client/src/components/Quiz/data.js
@@ -1,42 +1,306 @@
 export const data = [
     {
-        question: "What is the meaning of karela ??",
-        option1: "Karela",
-        option2: "Kerela",
-        option3: "Karala",
-        option4: "Karele",
-        ans: 1,
+    question: "What is the name of the cult in GTA V?",
+    option1: "The Brotherhood",
+    option2: "The Family",
+    option3: "The Order",
+    option4: "Epsilon Program",
+    ans: 4,
     },
     {
-        question: "What is the meaning of karela ????",
-        option1: "Karela",
-        option2: "Kerela",
-        option3: "Karala",
-        option4: "Karele",
-        ans: 3,
+    question: "Who is the leader of the Epsilon Program?",
+    option1: "Franklin Clinton",
+    option2: "Michael De Santa",
+    option3: "Trevor Phillips",
+    option4: "Cris Formage",
+    ans: 4,
     },
     {
-        question: "What is the meaning of karela ??????",
-        option1: "Karela",
-        option2: "Kerela",
-        option3: "Karala",
-        option4: "Karele",
-        ans: 1,
+    question: "What is the main goal of the Epsilon Program?",
+    option1: "To take over the world",
+    option2: "To gain wealth and power",
+    option3: "To find inner enlightenment",
+    option4: "To spread love and peace",
+    ans: 3,
     },
     {
-        question: "What is the meaning of karela ????????",
-        option1: "Karela",
-        option2: "Kerela",
-        option3: "Karala",
-        option4: "Karele",
-        ans: 2,
+    question: "What is the name of the Epsilon Program's website?",
+    option1: "epsilontruth.com",
+    option2: "epsiloncult.com",
+    option3: "epsilonlove.com",
+    option4: "epsilonprogram.com",
+    ans: 4,
     },
     {
-        question: "What is the meaning of karela ??????????",
-        option1: "Karela",
-        option2: "Kerela",
-        option3: "Karala",
-        option4: "Karele",
-        ans: 2,
-    }
+    question: "What is the Epsilon Program's symbol?",
+    option1: "A triangle",
+    option2: "A star",
+    option3: "A circle",
+    option4: "A heart",
+    ans: 3,
+    },
+    {
+    question: "What is the name of the Epsilon Program's meditation technique?",
+    option1: "Enlightenmentization",
+    option2: "Crisization",
+    option3: "Formageation",
+    option4: "Epsilonization",
+    ans: 4,
+    },
+    {
+    question: "What is the Epsilon Program's stance on material possessions?",
+    option1: "They are irrelevant",
+    option2: "They are a distraction from inner peace",
+    option3: "They are a means to an end",
+    option4: "They are essential for happiness",
+    ans: 2,
+    },
+    {
+    question: "Who is the Epsilon Program's main rival?",
+    option1: "The Vagos",
+    option2: "The Ballas",
+    option3: "The Families",
+    option4: "The Altruist Cult",
+    ans: 4,
+    },
+    {
+    question: "What is the name of the Epsilon Program's headquarters?",
+    option1: "The Inner Peace Sanctuary",
+    option2: "The Enlightenment Temple",
+    option3: "The Cris Formage Institute",
+    option4: "The Epsilon Center",
+    ans: 4,
+    },
+    {
+    question: "What is the Epsilon Program's view on technology?",
+    option1: "It is irrelevant",
+    option2: "It is a means to control people",
+    option3: "It is a distraction from inner peace",
+    option4: "It is a tool for enlightenment",
+    ans: 3,
+    },
+    {
+    question: "What is the name of the Epsilon Program's newsletter?",
+    option1: "The Epsilon Times",
+    option2: "The Cris Chronicle",
+    option3: "The Enlightenment Express",
+    option4: "The Inner Peace Insider",
+    ans: 1,
+    },
+    {
+    question: "What is the Epsilon Program's stance on violence?",
+    option1: "It is a necessary evil",
+    option2: "It is a means to an end",
+    option3: "It is a distraction from inner peace",
+    option4: "It is never justified",
+    ans: 3,
+    },
+    {
+    question: "Who is the Epsilon Program's most famous member?",
+    option1: "Cris Formage",
+    option2: "Michael De Santa",
+    option3: "Franklin Clinton",
+    option4: "Trevor Phillips",
+    ans: 1,
+    },
+    {
+        question: "What is the name of the cult in GTA V?",
+        option1: "Epsilon Program",
+        option2: "The Family",
+        option3: "The Order",
+        option4: "The Brotherhood",
+        ans: 1
+    },
+    {
+        question: "Who is the leader of the Epsilon Program?",
+        option1: "Cris Formage",
+        option2: "Michael De Santa",
+        option3: "Trevor Philips",
+        option4: "Lamar Davis",
+        ans: 1
+    },
+    {
+        question: "What is the sacred color of the Epsilon Program?",
+        option1: "Blue",
+        option2: "Red",
+        option3: "Green",
+        option4: "Yellow",
+        ans: 1
+    },
+    {
+        question: "Which in-game character joins the Epsilon Program?",
+        option1: "Michael De Santa",
+        option2: "Franklin Clinton",
+        option3: "Trevor Philips",
+        option4: "Lester Crest",
+        ans: 1
+    },
+    {
+        question: "What is the ultimate goal of the Epsilon Program?",
+        option1: "Enlightenment",
+        option2: "World domination",
+        option3: "Wealth",
+        option4: "Power",
+        ans: 1
+    },
+    {
+        question: "What is the Epsilon Program's slogan?",
+        option1: "Kifflom",
+        option2: "Live long and prosper",
+        option3: "In unity, there is strength",
+        option4: "Power to the people",
+        ans: 1
+    },
+    {
+        question: "How much does it cost to join the Epsilon Program in GTA V?",
+        option1: "$5,000",
+        option2: "$10,000",
+        option3: "$15,000",
+        option4: "$25,000",
+        ans: 4
+    },
+    {
+        question: "What do members of the Epsilon Program wear?",
+        option1: "Blue robes",
+        option2: "White suits",
+        option3: "Red jackets",
+        option4: "Black uniforms",
+        ans: 1
+    },
+    {
+        question: "Where is the Epsilon Center located in GTA V?",
+        option1: "Rockford Hills",
+        option2: "Vinewood",
+        option3: "Sandy Shores",
+        option4: "Paleto Bay",
+        ans: 1
+    },
+    {
+        question: "What vehicle is given to Michael during the Epsilon missions?",
+        option1: "Declasse Tornado",
+        option2: "Pegassi Vacca",
+        option3: "Truffade Z-Type",
+        option4: "Albany Emperor",
+        ans: 1
+    },
+    {
+        question: "What do members of the Epsilon Program believe in?",
+        option1: "Cris Formage is a deity",
+        option2: "Aliens",
+        option3: "Money and power",
+        option4: "Eternal happiness",
+        ans: 1
+    },
+    {
+        question: "What is the greeting used by Epsilon members?",
+        option1: "Kifflom",
+        option2: "Namaste",
+        option3: "Hello Brother",
+        option4: "Peace be with you",
+        ans: 1
+    },
+    {
+        question: "What phrase do Epsilon members say to one another?",
+        option1: "Kifflom",
+        option2: "Blessed be",
+        option3: "Peace and love",
+        option4: "In brotherhood",
+        ans: 1
+    },
+    {
+        question: "What is the Epsilon Program's sacred text?",
+        option1: "The Epsilon Tract",
+        option2: "The Holy Bible",
+        option3: "The Koran",
+        option4: "The Book of Mormon",
+        ans: 1
+    },
+    {
+        question: "What animal is sacred to the Epsilon Program?",
+        option1: "Squirrel",
+        option2: "Cat",
+        option3: "Dog",
+        option4: "Cow",
+        ans: 1
+    },
+    {
+        question: "How many tenets does the Epsilon Program have?",
+        option1: "12",
+        option2: "9",
+        option3: "10",
+        option4: "7",
+        ans: 1
+    },
+    {
+        question: "What does Epsilonism teach about the past?",
+        option1: "It is a lie",
+        option2: "It is sacred",
+        option3: "It is a mystery",
+        option4: "It is forgotten",
+        ans: 1
+    },
+    {
+        question: "What task is given to Michael by the Epsilon Program?",
+        option1: "Collecting alien artifacts",
+        option2: "Stealing cars",
+        option3: "Recruiting new members",
+        option4: "Finding hidden glyphs",
+        ans: 1
+    },
+    {
+        question: "What is the first mission Michael does for the Epsilon Program?",
+        option1: "Accepting the Truth",
+        option2: "Seeking the Truth",
+        option3: "Bearing the Truth",
+        option4: "Delivering the Truth",
+        ans: 2
+    },
+    {
+        question: "What vehicle does Michael have to deliver in the mission 'Exercising the Truth'?",
+        option1: "A red car",
+        option2: "A blue car",
+        option3: "A green car",
+        option4: "A yellow car",
+        ans: 1
+    },
+    {
+        question: "What does Michael have to do during 'Bearing the Truth'?",
+        option1: "Wear Epsilon robes for 10 days",
+        option2: "Steal cars",
+        option3: "Find artifacts",
+        option4: "Donate money",
+        ans: 1
+    },
+    {
+        question: "What do Epsilon members believe about the solar system?",
+        option1: "It is the center of the universe",
+        option2: "It is an illusion",
+        option3: "It is sacred",
+        option4: "It is dangerous",
+        ans: 2
+    },
+    {
+        question: "How many paths to enlightenment are there in the Epsilon Program?",
+        option1: "9",
+        option2: "7",
+        option3: "5",
+        option4: "10",
+        ans: 1
+    },
+    {
+        question: "What does Michael need to find in 'Unknowing the Truth'?",
+        option1: "The Epsilon Tractor",
+        option2: "The Epsilon Sword",
+        option3: "The Epsilon Map",
+        option4: "The Epsilon Orb",
+        ans: 1
+    },
+    {
+        question: "What is the reward for completing all Epsilon missions?",
+        option1: "$2,100,000",
+        option2: "$1,000,000",
+        option3: "$500,000",
+        option4: "$10,000",
+        ans: 1
+    },
 ]
