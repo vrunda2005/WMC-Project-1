289c646916c0bf993c33475f26c30784b2ce8dad
Vrunda Patel <vrunda.p@ahduni.edu.in>
2024-07-26 11:32:08 +0530

    commit_1

--- react_frontend/Client/src/components/Navbar/Navbar.jsx
+++ react_frontend/Client/src/components/Navbar/Navbar.jsx
@@ -1,109 +1,120 @@
-import React, { useContext, useState, useEffect } from 'react';
-import './Navbar.css';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { FaRegUser } from "react-icons/fa";
-import { MdCurrencyBitcoin } from "react-icons/md";
+import { FaRegUser, FaSun, FaMoon, FaBars, FaTimes } from 'react-icons/fa';
+import { MdCurrencyBitcoin } from 'react-icons/md';
 import { useAuth } from '../../creatContext';
-import MembershipTier from '../Membership/epsilon_program_membership'
 import { useTheme } from '../../usetheamContext';
-import { FaSun, FaMoon } from 'react-icons/fa';
-
-
+import './Navbar.css';
 
 const Navbar = () => {
   const navigate = useNavigate();
-  const [auth,setAuth]=useAuth();
+  const [auth, setAuth] = useAuth();
   const { theme, toggleTheme } = useTheme();
-  const [userData, setUserData]=useAuth();
-  
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
   const handleLogout = () => {
-    alert("You have logged out")
+    alert("You have logged out");
     localStorage.removeItem('auth');
     setAuth({ user: null, token: '', isLoggedIn: false });
     navigate('/login', { replace: true });
   };
 
-  // Apply theme classes
-  const headerClass = `sticky z-50 top-0 p-5 flex justify-between ${theme === 'blue' ? 'bg-blue-primary-bg text-blue-text-light' : 'bg-dark-primary-bg text-dark-text-light'}`;
+  const headerClass = `sticky z-50 top-0 p-5 flex justify-between items-center ${theme === 'blue' ? 'bg-blue-primary-bg text-blue-text-light' : 'bg-dark-primary-bg text-dark-text-light'}`;
   const buttonClass = `p-2 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 ${
     theme === 'blue'
       ? 'bg-blue-highlight hover:bg-blue-highlight-dark text-white'
       : 'bg-dark-highlight hover:bg-dark-highlight-dark text-white'
   } transition duration-300 ease-in-out`;
-  
+
   return (
     <header className={headerClass}>
-      <a href="/" className="logo">Logo</a>
+      <a href="/" className="logo text-lg font-bold">Logo</a>
       <button onClick={toggleTheme} className={buttonClass}>
-      {theme === 'dark' ? <FaSun className="text-yellow-500" /> : <FaMoon className="text-gray-500" />}
-    </button>
-      {auth.membership_id}
-      {
-        auth.isLoggedIn ? (
-          <nav className="navbar flex flex-wrap justify-between md:flex-row flex-col">
-            <div className="flex items-center mb-2 md:mb-0">
-              <FaRegUser size="1.5em" />
-              <span className="ml-2">{auth.username}</span>
-            </div>
-            <div className="flex items-center mb-2 md:mb-0">
-              <MdCurrencyBitcoin size="1.5em" />
-              <span className="ml-2">{auth.userPoints}</span>
-            </div>
-            <ul className="flex flex-wrap justify-between md:flex-row flex-col">
-              <li><Link to="/">Home</Link></li>
-              <li>
-                <Link to={auth.membership_id ? `/epsilon_program_membership/${auth.membership_id}` : '/membership'}>
-                  Membership
-                </Link>
-              </li>
-              <li><Link to="/storyPage">Stories</Link></li>
-              <li><Link to="/Blog">Blog</Link></li>
-              <li><Link to="/Donate">Donate</Link></li>
-              <li><Link to='/About'>About</Link></li>
-              <li><Link to='/Quiz'>Quiz</Link></li>
-              <li><Link to='/epsilonMap'>EpsilonMap</Link></li>
-              {
-                auth.isAdmin ? (
-                  <li><Link to='/events'>Events</Link></li>
-                ) : null
-              }
-            </ul>
-          </nav>
-        ) : (
-          <nav className="navbar flex flex-wrap justify-between md:flex-row flex-col">
-            <ul className="flex flex-wrap justify-between md:flex-row flex-col">
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/blog">Blog</Link></li>
-              <li><Link to='/About'>About</Link></li>
-              <li><Link to='/Quiz'>Quiz</Link></li>
-            </ul>
-          </nav>
-        )
-      }
+        {theme === 'dark' ? <FaSun className="text-yellow-500" /> : <FaMoon className="text-gray-500" />}
+      </button>
 
-      {auth.isLoggedIn ? (
-        <div className="flex items-center lg:order-2">
-          <button onClick={handleLogout} className={`text-gray-800 hover:bg-red-500
-           focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none ${buttonClass}`}>
-            Logout
+      {/* Mobile Menu Toggle Button */}
+      <div className="md:hidden flex items-center">
+        <button onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} className={buttonClass} aria-label="Toggle Menu">
+          {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
+        </button>
+      </div>
+
+      {/* Mobile Menu Content */}
+      <nav className={`md:hidden fixed inset-0 bg-white dark:bg-gray-800 p-5 ${isMobileMenuOpen ? 'block' : 'hidden'}`}>
+        <div className="flex justify-between items-center mb-4">
+          <a href="/" className="logo text-lg font-bold">Logo</a>
+          <button onClick={() => setIsMobileMenuOpen(false)} className={buttonClass} aria-label="Close Menu">
+            <FaTimes />
           </button>
         </div>
-      ) : (
-        <div className="flex items-center lg:order-2">
-          <Link
-              to="/login"
-              className={`text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none ${buttonClass}`}
-          >
+        <ul className="flex flex-col space-y-4">
+          <li><Link to="/" onClick={() => setIsMobileMenuOpen(false)}>Home</Link></li>
+          <li><Link to={auth.membership_id ? `/epsilon_program_membership/${auth.membership_id}` : '/membership'} onClick={() => setIsMobileMenuOpen(false)}>Membership</Link></li>
+          <li><Link to="/storyPage" onClick={() => setIsMobileMenuOpen(false)}>Stories</Link></li>
+          <li><Link to="/Blog" onClick={() => setIsMobileMenuOpen(false)}>Blog</Link></li>
+          <li><Link to="/Donate" onClick={() => setIsMobileMenuOpen(false)}>Donate</Link></li>
+          <li><Link to='/About' onClick={() => setIsMobileMenuOpen(false)}>About</Link></li>
+          <li><Link to='/Quiz' onClick={() => setIsMobileMenuOpen(false)}>Quiz</Link></li>
+          <li><Link to='/epsilonMap' onClick={() => setIsMobileMenuOpen(false)}>EpsilonMap</Link></li>
+          {auth.isAdmin && <li><Link to='/events' onClick={() => setIsMobileMenuOpen(false)}>Events</Link></li>}
+        </ul>
+        {auth.isLoggedIn ? (
+          <button onClick={handleLogout} className={`mt-4 text-gray-800 hover:bg-red-500 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 ${buttonClass}`}>
+            Logout
+          </button>
+        ) : (
+          <div className="mt-4 flex flex-col space-y-2">
+            <Link to="/login" className={`text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 ${buttonClass}`}>
               Log in
-          </Link>
-          <Link
-              to="/signup"
-              className={buttonClass}
-          >
+            </Link>
+            <Link to="/signup" className={buttonClass}>
               Get started
-          </Link>
-        </div>
-      )}
+            </Link>
+          </div>
+        )}
+      </nav>
+
+      {/* Desktop Menu Content */}
+      <nav className="hidden md:flex flex-wrap justify-between items-center">
+        {auth.isLoggedIn && (
+          <div className="flex items-center mb-2">
+            <FaRegUser size="1.5em" />
+            <span className="ml-2">{auth.username}</span>
+          </div>
+        )}
+        {auth.isLoggedIn && (
+          <div className="flex items-center mb-2">
+            <MdCurrencyBitcoin size="1.5em" />
+            <span className="ml-2">{auth.userPoints}</span>
+          </div>
+        )}
+        <ul className="flex flex-wrap justify-between">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to={auth.membership_id ? `/epsilon_program_membership/${auth.membership_id}` : '/membership'}>Membership</Link></li>
+          <li><Link to="/storyPage">Stories</Link></li>
+          <li><Link to="/Blog">Blog</Link></li>
+          <li><Link to="/Donate">Donate</Link></li>
+          <li><Link to='/About'>About</Link></li>
+          <li><Link to='/Quiz'>Quiz</Link></li>
+          <li><Link to='/epsilonMap'>EpsilonMap</Link></li>
+          {auth.isAdmin && <li><Link to='/events'>Events</Link></li>}
+        </ul>
+        {auth.isLoggedIn ? (
+          <button onClick={handleLogout} className={`text-gray-800 hover:bg-red-500 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 ${buttonClass}`}>
+            Logout
+          </button>
+        ) : (
+          <div className="flex items-center">
+            <Link to="/login" className={`text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 mr-2 ${buttonClass}`}>
+              Log in
+            </Link>
+            <Link to="/signup" className={buttonClass}>
+              Get started
+            </Link>
+          </div>
+        )}
+      </nav>
     </header>
   );
 };
--- react_frontend/Client/src/components/Signup/SignUp.jsx
+++ react_frontend/Client/src/components/Signup/SignUp.jsx
@@ -3,99 +3,120 @@ import axios from "axios";
 
 export default function App() {
   const [value, setValue] = useState({
-    name:'',
-    email:'',
-    password:''
+    name: '',
+    email: '',
+    password: ''
   });
 
   const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+
   const handleShowPassword = () => {
     setShowPassword(!showPassword);
   };
 
   const handleChange = (e) => {
-    setValue ({
-        ...value,
-        [e.target.name]: e.target.value
+    setValue({
+      ...value,
+      [e.target.name]: e.target.value
     });
   };
 
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const reigter = await axios.post("https://wmc-project-av5d.onrender.com/register", value);
-    console.log(reigter.data);
-    setValue({
-      name: "",
-      email: "",
-      password: "",
-    });
-    alert("Acount created");
+    setError(''); // Clear previous errors
+    setSuccess(''); // Clear previous success message
+
+    try {
+      const response = await axios.post("https://wmc-project-av5d.onrender.com/register", value);
+      console.log(response.data);
+      setValue({
+        name: "",
+        email: "",
+        password: "",
+      });
+      setSuccess("Account created successfully!");
+    } catch (err) {
+      if (err.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        setError(`Error ${err.response.status}: ${err.response.data.message || 'An error occurred'}`);
+      } else if (err.request) {
+        // The request was made but no response was received
+        setError('No response received from the server');
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        setError('Error setting up the request');
+      }
+    }
   };
 
   return (
     <>
-    <div className="h-[600px] flex justify-center items-center bg-gray-100">
-      <div className="max-w-md h-fit w-full p-6 bg-white rounded shadow-md">
-        <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
-          <img
+      <div className="h-[600px] flex justify-center items-center bg-gray-100">
+        <div className="max-w-md h-fit w-full p-6 bg-white rounded shadow-md">
+          <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
+            <img
               src="https://upload.wikimedia.org/wikipedia/commons/4/47/Epsilon_Program_Logo.png"
               className="h-36 shadow-sm"
               alt="Logo"
-          />
-          <h2 className="text-5xl font-bold mt-2 mb-8 text-center">Sign Up</h2>
+            />
+            <h2 className="text-5xl font-bold mt-2 mb-8 text-center">Sign Up</h2>
 
-          <input 
-            placeholder="Username" 
-            value={value.name} 
-            onChange={handleChange} 
-            required 
-            name= "name" 
-            className="w-full p-2 pl-5 text-sm text-gray-700 border border-gray-300 rounded"
-          />
+            {error && <div className="text-red-500 mb-4">{error}</div>}
+            {success && <div className="text-green-500 mb-4">{success}</div>}
 
-          <input 
-            type="email"
-            placeholder="Email" 
-            value={value.email} 
-            onChange={handleChange} 
-            required 
-            name= "email" 
-            className="w-full p-2 pl-5 text-sm text-gray-700 border border-gray-300 rounded mt-4"
-          />
-          
-          <div className="flex gap-2">
             <input 
-              type={showPassword ? "text" : "password"}
-              placeholder="Password" 
-              value={value.password} 
+              placeholder="Username" 
+              value={value.name} 
               onChange={handleChange} 
               required 
-              name= "password" 
-              className="w-full p-2 pl-5 text-sm text-gray-700 border border-gray-300 rounded mt-4"
+              name="name" 
+              className="w-full p-2 pl-5 text-sm text-gray-700 border border-gray-300 rounded"
             />
 
+            <input 
+              type="email"
+              placeholder="Email" 
+              value={value.email} 
+              onChange={handleChange} 
+              required 
+              name="email" 
+              className="w-full p-2 pl-5 text-sm text-gray-700 border border-gray-300 rounded mt-4"
+            />
+            
+            <div className="flex gap-2">
+              <input 
+                type={showPassword ? "text" : "password"}
+                placeholder="Password" 
+                value={value.password} 
+                onChange={handleChange} 
+                required 
+                name="password" 
+                className="w-full p-2 pl-5 text-sm text-gray-700 border border-gray-300 rounded mt-4"
+              />
+
+              <button 
+                type="button"
+                onClick={handleShowPassword} 
+                className="border w-10 border-gray-300 rounded mt-4 p-1 hover:bg-gray-50"
+              >
+                {showPassword ? "◎" : "◉" }
+              </button>
+            </div>
+
             <button 
-              type="button"
-              onClick={handleShowPassword} 
-              className="border w-10 border-gray-300 rounded mt-4 p-1 hover:bg-gray-50"
+              type="submit" 
+              className="w-full bg-orange-500 hover:bg-orange-600 transition-all text-white font-bold py-2 px-4 rounded mt-4"
             >
-              {showPassword ? "◎" : "◉" }
+              Submit
             </button>
-          </div>
 
-          <button 
-            type="submit" 
-            className="w-full bg-orange-500 hover:bg-orange-600 transition-all text-white font-bold py-2 px-4 rounded mt-4"
-          >
-            Submit
-          </button>
-
-          <h4 className='mt-4 text-center'> Already have an account? <a href='\login' className='text-blue-500 font-bold hover:cursor-pointer hover:text-blue-400 transition-all'>Log In</a> </h4>
-          
-        </form>
+            <h4 className='mt-4 text-center'>Already have an account? <a href='\login' className='text-blue-500 font-bold hover:cursor-pointer hover:text-blue-400 transition-all'>Log In</a></h4>
+          </form>
+        </div>
       </div>
-    </div>
     </>
   );
 }
