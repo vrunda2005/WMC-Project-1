2898032bc3d61cd217af45273803a58698e8f7b4
Vrunda Patel <vrunda.p@ahduni.edu.in>
2024-07-27 00:30:35 +0530

    img

--- react_frontend/Client/src/components/Membership/epsilon_program_membership.jsx
+++ react_frontend/Client/src/components/Membership/epsilon_program_membership.jsx
@@ -30,6 +30,7 @@ const MembershipTier = ({ tier, benefits, profileLink, newsLink, message, onCanc
       </ul>
         </div>
         <div class="column">
+          <img className='w-[300px] h-[300px] mx-auto my-4 rounded-full animate-pulse' src="https://i.ytimg.com/vi/4pPNTzlfxsk/maxresdefault.jpg" alt="img" />
         </div>
     </section>
     
@@ -66,16 +67,14 @@ const MembershipTier = ({ tier, benefits, profileLink, newsLink, message, onCanc
     <div class="column text">
     {message}
     </div>
-  </section>
-  <button
+    <button
           className={`bg-blue-text-dark hover:bg-red-500 ${textPrimary} font-bold py-2 px-4 rounded-lg transition-all`}
           onClick={onCancelMembership}
         >
           Cancel Membership
         </button>
-  <section class="color ss-style-bigtriangle">
-
   </section>
+
   <svg id="bigTriangleColor" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 102" preserveAspectRatio="none">
         <path d="M0 0 L50 100 L100 0 Z" />
       </svg>
