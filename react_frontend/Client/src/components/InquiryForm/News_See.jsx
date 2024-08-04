3afccd67e264e94ce64556b159fd71b1a6c523b6
Vrunda Patel <vrunda.p@ahduni.edu.in>
2024-08-04 14:31:07 +0530

    membership

--- react_frontend/Client/src/components/InquiryForm/News_See.jsx
+++ react_frontend/Client/src/components/InquiryForm/News_See.jsx
@@ -31,12 +31,12 @@ const UserNews = () => {
 
   return (
     <>
-      <div className="p-6 h-screen page-background bg-opacity-10">
+      <div className="p-6  page-background bg-opacity-10 text-center">
         <h1 className="font-bold mb-6 text-2xl">Epsilon Program News</h1>
         {news.length === 0 ? (
           <p>No news available.</p>
         ) : (
-          <div className="flex flex-wrap gap-8">
+          <div className="flex flex-wrap gap-8 ">
             {news.map((item) => (
               <div key={item.id} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 flex flex-col">
                 <a href="#" onClick={() => openModal(item)}>
@@ -72,7 +72,8 @@ const UserNews = () => {
 
       {isModalOpen && selectedNews && (
         <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50">
-          <div className="bg-white p-8 rounded-lg shadow-lg max-w-3xl mx-4 sm:mx-auto relative">
+               <h2 className="text-2xl font-bold mb-6">{selectedNews.title}</h2>
+          <div className="bg-white p-8 flex flex-center items-center justify-center rounded-lg shadow-lg max-w-3xl max-h-[90vh]  mx-4 sm:mx-auto overflow-y-auto relative gap-4">
             <button
               onClick={closeModal}
               className="absolute top-4 right-4 text-gray-600 hover:text-gray-900 focus:outline-none"
@@ -81,8 +82,8 @@ const UserNews = () => {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
               </svg>
             </button>
-            <h2 className="text-3xl font-bold mb-6">{selectedNews.title}</h2>
-            <img className="w-full h-auto mb-6 rounded-lg" src={selectedNews.image} alt={selectedNews.title} />
+       
+            <img className="w-48 h-48 object-cover mb-6 rounded-lg" src={selectedNews.image} alt={selectedNews.title} />
             <p className="text-lg text-gray-700">{selectedNews.description}</p>
           </div>
         </div>
